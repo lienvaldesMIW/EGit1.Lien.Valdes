566662e50d43e745fde253129b26aee8056e3084
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-28 16:30:20 +0100

    CR1.03

--- src/clases/Productos.java
+++ src/clases/Productos.java
@@ -4,4 +4,18 @@ public class Productos {
 	private String nombre;
 	private int id;
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 }
