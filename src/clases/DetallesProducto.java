904b2fe184f3b7584f006acd3d87103ca71ea51a
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-28 17:33:47 +0100

    CD13

--- src/clases/DetallesProducto.java
+++ src/clases/DetallesProducto.java
@@ -2,4 +2,13 @@ package clases;
 
 public class DetallesProducto {
 	private String detalles;
+
+	public String getDetalles() {
+		return detalles;
+	}
+
+	public void setDetalles(String detalles) {
+		this.detalles = detalles;
+	}	
+	
 }
